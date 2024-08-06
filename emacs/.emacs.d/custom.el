9b7d87183d0077c4bfb499721a8a44df85fa95c8
Peter Gottesman <peter@petergottesman.com>
2024-08-06 14:52:48 -0400

    Update custom.el

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -13,7 +13,7 @@
    '("./Inc" "." "/usr/include/glib-2.0" "/usr/lib64/glib-2.0/include"))
  '(company-clang-arguments
    '("-I./inc" "-I../inc" "-I/usr/include/glib-2.0" "-I/usr/lib64/glib-2.0/include"))
- '(company-show-numbers t)
+ '(company-show-quick-access t)
  '(company-statistics-mode t)
  '(company-transformers '(company-sort-by-statistics))
  '(custom-safe-themes
@@ -22,13 +22,13 @@
  '(linum-disabled-modes-list
    '(eshell-mode wl-summary-mode compilation-mode org-mode text-mode dired-mode doc-view-mode image-mode pdf-view-mode))
  '(package-selected-packages
-   '(lua-mode org-ref annotate julia-mode rmsbolt disaster company-lsp company yasnippet helm-projectile helm evil-leader evil edbi edbi-database-url edbi-django edbi-sqlite emacsql emacsql-sqlite3 rjsx-mode tuareg bison-mode tex-site acutex auctex cuda-mode switch-window ivy-explorer counsel swiper ccls projectile lsp-treemacs flycheck lsp-ui lsp-mode yaml-mode counsel-gtags glsl-mode elpy pdf-tools gh-md markdown-mode company-go go-mode origami company-statistics company-quickhelp company-c-headers use-package multi-term paganini-theme hl-todo magit package-build))
+   '(eglot conda ninja-mode diffview lua-mode org-ref annotate julia-mode rmsbolt disaster company-lsp company yasnippet helm-projectile helm evil-leader evil edbi edbi-database-url edbi-django edbi-sqlite emacsql emacsql-sqlite3 rjsx-mode tuareg bison-mode tex-site acutex auctex cuda-mode switch-window ivy-explorer counsel swiper projectile lsp-treemacs flycheck lsp-ui lsp-mode yaml-mode counsel-gtags glsl-mode elpy pdf-tools gh-md markdown-mode company-go go-mode origami company-statistics company-quickhelp company-c-headers use-package multi-term paganini-theme hl-todo magit package-build))
  '(safe-local-variable-values
    '((indent-tabs-mode nil)
-	 (tab-width 2)
-	 (c-basic-offset 2)
-	 (company-clang-arguments "-I/usr/include" "-I../Inc" "-I../inc")
-	 (company-clang-arguments "-I/usr/include" "-I../Inc" "-I../Drivers/STM32F4xx_HAL_Driver/Inc" "-I../Drivers/CMSIS/Include" "-I../Drivers/CMSIS/Device/ST/STM32F4xx/Include" "-DUSE_HAL_DRIVER" "-DSTM32F410Rx"))))
+     (tab-width 2)
+     (c-basic-offset 2)
+     (company-clang-arguments "-I/usr/include" "-I../Inc" "-I../inc")
+     (company-clang-arguments "-I/usr/include" "-I../Inc" "-I../Drivers/STM32F4xx_HAL_Driver/Inc" "-I../Drivers/CMSIS/Include" "-I../Drivers/CMSIS/Device/ST/STM32F4xx/Include" "-DUSE_HAL_DRIVER" "-DSTM32F410Rx"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
